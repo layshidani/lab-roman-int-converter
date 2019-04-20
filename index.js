f96f950c9ab6810b00a331019d2bdf6251a7993f
hlays <lays.hidani@gmail.com>
2019-04-20 00:36:01 -0300

    add validaçao de input

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 const integers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
+console.log('==== Olá, este é o conversor de Algorismos Romanos e Números Inteiros ====');
+console.log('');
+
+const isValidRoman = function validateRoman(userInput) {
+  if (typeof userInput !== 'string' || userInput === '') {
+    return false;
+  }
+};
+
+const isValidNumber = function validateNumber(userInput) {
+  if (typeof userInput !== 'int' || userInput === 0) {
+    return false;
+  }
+}
+
 function romanToInt(roman) {
   roman = roman.toUpperCase();
   let result = 0;
+
   for (let i in integers) {
     while (roman.indexOf(romans[i]) === 0) {
       result += integers[i];
       roman = roman.replace(romans[i], '');
     }
   }
-  return result;
-}
 
-console.log('romanToInt(XxiV): ', romanToInt('XxiV'));
+  if (!isValidRoman || result < 1) {
+    return ':/ Algarismo inválido. Tente de novo...';
+  } else {
+    return 'Resultado: ' + result;
+  }
+} 
 
 function intToRoman(number) {
   let result = '';
@@ -23,7 +42,16 @@ function intToRoman(number) {
       number -= integers[j];
     }
   }
-  return result;
+
+  if (!isValidRoman || result < 1) {
+    return ':/ Número inválido. Tente de novo...';
+  } else {
+    return 'Resultado: ' + result;
+  }
 }
 
-console.log('intToRoman(24): ', intToRoman(24));
+console.log('romanToInt(xxvi): ', romanToInt('xxvi'));
+console.log('romanToInt(): ', romanToInt(''));
+console.log('intToRoman(28): ', intToRoman(28));
+console.log('intToRoman(0): ', intToRoman(0));
+console.log('intToRoman(): ', intToRoman());
