0eb2ada91416e0f4b011a9082ad133e00e2a9775
hlays <lays.hidani@gmail.com>
2019-04-22 14:51:59 -0300

    add new linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,58 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
-    },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'camelcase': 2,
+    'keyword-spacing': 1,
+    'comma-spacing': 1,
+    // 'space-before-blocks': 1,
+    // 'spaced-comment': 1,
+    // 'key-spacing': [
+    //   'error',
+    //   {
+    //     'beforeColon': false
+    //   }
+    // ],
+    // 'no-multi-spaces': 'error',
+    // 'space-in-parens': [
+    //   'error',
+    //   'always'
+    // ],
+    // // 'space-before-function-paren': [
+    // //   'error',
+    // // ],
+    // 'func-call-spacing': [
+    //   'error',
+    //   'never'
+    // ],
+    // 'no-multiple-empty-lines':
+    //   'error',
+  }
 };
