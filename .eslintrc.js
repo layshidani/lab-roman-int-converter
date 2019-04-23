80acc7501edc0b81bc244d261e234eaaaed4734d
hlays <lays.hidani@gmail.com>
2019-04-23 16:00:53 -0300

    update linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,39 @@
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es6': true,
-    'node': true
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
   },
-  'extends': 'eslint:recommended',
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  'parserOptions': {
-    'ecmaVersion': 2018
+  parserOptions: {
+    ecmaVersion: 2018
   },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ],
-    'camelcase': 2,
-    'keyword-spacing': 1,
-    'comma-spacing': 1,
-    // 'space-before-blocks': 1,
-    // 'spaced-comment': 1,
-    // 'key-spacing': [
-    //   'error',
-    //   {
-    //     'beforeColon': false
-    //   }
-    // ],
-    // 'no-multi-spaces': 'error',
-    // 'space-in-parens': [
-    //   'error',
-    //   'always'
-    // ],
-    // // 'space-before-function-paren': [
-    // //   'error',
-    // // ],
-    // 'func-call-spacing': [
-    //   'error',
-    //   'never'
-    // ],
-    // 'no-multiple-empty-lines':
-    //   'error',
+  rules: {
+    "keyword-spacing": 1,
+    "space-before-function-paren": [1, "never"],
+    "eqeqeq": 1,
+    "space-infix-ops": 1,
+    "comma-spacing": 1,
+    "brace-style": 1,
+    "no-multiple-empty-lines": 1,
+    "camelcase": 1,
+    "func-call-spacing": 1,
+    "key-spacing": 1,
+    "semi": 1,
+    "no-floating-decimal": 1,
+    "no-multi-spaces": 1,
+    "object-property-newline": 1,
+    "padded-blocks": [1, "never"],
+    "space-before-blocks": 1,
+    "space-in-parens": 1,
+    "spaced-comment": 1,
+    "quotes": [1, "single"],
+    "id-length": [1, { "exceptions": ["i", "j", "x"] }],
+    "indent": [1, 2],
+    "no-array-constructor": 1
   }
 };
